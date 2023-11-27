6623120c63def614636b82a8ab58b08a7e83d6d5
C14210224 <107255810+C14210224@users.noreply.github.com>
2023-11-27 12:32:17 +0700

    percobaan6

--- app/src/main/java/com/alloys/e_tix/MainActivity.kt
+++ app/src/main/java/com/alloys/e_tix/MainActivity.kt
@@ -7,20 +7,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        print("lorenzo ganteng")
-        print("lorenzo bangettt brooohh")
-        print("lorenzo ganteng")
-        print("lorenzo bangettt brooohh")
-        print("kjdbgsbjkhbgksjbgs")
+        print("beberqpa anagka")
+        print("beberqpa anagka")
 
-        print("lorenzo ganteng")
-        print("lorenzo bangettt brooohh")
-        print("lorenzo ganteng")
 
-        if (true) {
-            print("halo")
-        }
-        print("lorenzo bangettt brooohh")
-        print("kjdbgsbjkhbgksjbgs")
+        print("beberqpa anagka")
+
+
+        print("beberqpa anagka")
+        print("beberqpa anagka")
+
     }
 }
