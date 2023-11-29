2264ec8ee813fb520369419e1753c8aca1f86e0a
Steven Arnold <c14210169@john.petra.ac.id>
2023-11-29 17:46:47 +0700

    halo

--- app/src/main/java/com/alloys/e_tix/shortcutPageDev.kt
+++ app/src/main/java/com/alloys/e_tix/shortcutPageDev.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.Button
 
 class shortcutPageDev : AppCompatActivity() {
+//    halo
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shortcut_page_dev)
