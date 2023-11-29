c754ef249b788c2e10789c16bf977180f000539b
Alloy-S <c14210265@john.petra.ac.id>
2023-11-29 17:47:42 +0700

    coba stach

--- app/src/main/java/com/alloys/e_tix/shortcutPageDev.kt
+++ app/src/main/java/com/alloys/e_tix/shortcutPageDev.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.Button
 
 class shortcutPageDev : AppCompatActivity() {
+//    hehehehhehe
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shortcut_page_dev)
