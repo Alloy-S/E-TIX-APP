7bcc269aefc759ee429f9b3231ac179a9e18ae54
C14210224 <107255810+C14210224@users.noreply.github.com>
2023-12-03 18:50:24 +0700

    history + detail

---
+++ app/src/main/java/com/alloys/e_tix/dataHistory.kt
@@ -0,0 +1,20 @@
+package com.alloys.e_tix
+
+import android.media.Image
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import java.util.Date
+
+data class dataHistory(
+    var posterFilm : Int,
+    var date : String,
+    var time : String,
+    var movie : String,
+    var mall : String,
+    var code : String,
+    var dateShow : String,
+    var timeShow : String,
+    var barcode : String,
+    var detail : String
+)
